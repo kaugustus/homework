5b0d95fdff5f9b0602a43b538a471c92089add5c
vagrant <vagrant@kellogg-dev-box>
2014-04-22 03:38:28 +0000

    HW 1 step 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421231442) do
+ActiveRecord::Schema.define(version: 20140422032809) do
+
+  create_table "books", force: true do |t|
+    t.string "title"
+    t.string "author"
+  end
 
   create_table "rolls", force: true do |t|
     t.integer "rollone"
